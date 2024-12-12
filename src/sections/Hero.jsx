6bd8495044f2db1efc41b6250c9a05e4a2e6349f
ryalfarissi <fahrialfarissi2004@gmail.com>
2024-12-12 14:44:54 +0700

    first commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <section id="home" className=" w-full flex xl:flex-row flex-col justify-center min-h-screen p-2 gap-20 max-container">
             <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
                 <p className="ml-1  text-2xl font-montserrat text-primary-kalia font-bold">Description</p>
-                <h1 className=" font-palanquin text-8xl max-sm:text-[72px] font-bold max-sm:leading-[82px]">CV. KALIA UTAMA NUSANTARA</h1>
+                <h1 className=" font-palanquin text-8xl max-sm:text-4xl font-bold max-sm:leading-[82px]">CV. KALIA UTAMA NUSANTARA</h1>
                 <p className="font-montserrat text-slate-gray text-lg leading-7 mt-6 mb-14 sm:max-w-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati perspiciatis laborum voluptate, quos nostrum perferendis omnis odio sapiente corrupti quisquam.</p>
                 <Button label="Contact Us" />
             </div>
